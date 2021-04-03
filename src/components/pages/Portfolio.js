dff8d63bd1710c409a12da51e09996a99497cccf
Olivia Park <oliviaypark113@gmail.com>
2021-04-02 21:35:13 -0400

    updated about contact portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,21 +1,259 @@
 import React from "react";
+import "../css/portfolio.css";
 
 const Portfolio = () => (
-  <div>
-    <h1>Portfolio Page</h1>
-    <p>
-      Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-      turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-      Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-      nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi justo
-      laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-      lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec placerat
-      accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque vehicula sit
-      amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus non ullamcorper
-      iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-    </p>
+
+  <div className="container">
+  <div className="row">
+      <div className="col-lg-12 col-md-12 col-sm-12 portfolio-mainbox">
+          <div className="row">
+              <div className="col-lg-12 col-sm-12">
+                  <h1 id="portfolio">Recent Project</h1>
+              </div>
+          </div>
+
+          <div className="row">
+              <h3 className="project-title">CODE CLOSET<br/><span id="service">Code </span>UNTIL YOU'RE COLD</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="demo/fullview.gif" alt="code closet-2 full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/closet_responsive_img.jpg" alt="code closet-2 responsive"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">EXPRESS HANDLEBARS, CSS, BOOTSTRAP, JAVASCRIPT, EXPRESS, SEQUELIZE, NODE JS </p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://stormy-stream-41757.herokuapp.com/" target="_blank"
+                      title="code-closet SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/CodeCloset-P2" target="_blank"
+                      title="code-closet CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+          <div className="row">
+              <h3 className="project-title">Eat<br/><span id="service">DA </span>BURGERS</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="demo/demo 2.gif" alt="eat-da burger full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/eatdaburger_responsive_img.jpg" alt="eat-da burger responsive"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">EXPRESS HANDLEBARS, CSS, BOOTSTRAP, JAVASCRIPT, EXPRESS, MYSQL, NODE JS </p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://evening-castle-79713.herokuapp.com/" target="_blank"
+                      title="eat-da burger SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/burgerapp_13" target="_blank"
+                      title="eat-da burger CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+
+          <div className="row">
+              <h3 className="project-title">NOTE<br/><span id="service">Daily </span>TAKER</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="demo/demo3.gif" alt="note taker full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/notetaker_responsive_img.jpg" alt="note-taker responsive"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">HTML, CSS, JAVASCRIPT, EXPRESS, NODE JS, NPM UNIQUID</p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://sheltered-falls-35987.herokuapp.com/" target="_blank"
+                      title="note-taker SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/note_taker_11" target="_blank"
+                      title="note-taker CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+
+
+          <div className="row">
+              <div className="col-lg-12">
+                  <h3 className="project-title">EMPLOYEE<br/><span id="service">Data </span>TRACKER</h3>
+              </div>
+          
+              <div className="col-lg-12 notetaker">
+                  <div className="notetaker-img">
+                      <img src="demo/demo4.gif" alt="note taker full screen" className=" img-thumbnail img-fluid"/>
+                  </div>
+              </div>    
+          
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques"> JAVASCRIPT, MYSQL, NODE JS, NPM INQUIRER</p>
+              </div>
+          
+              <div className="col-lg-12 col-sm-12 buttons">
+          
+                  <a href="https://github.com/Oliviapark113/employee_tracker_12" target="_blank" title="employee-tracker CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+
+          <div className="row">
+              <h3 className="project-title">WEATHER OR NOT<br/><span id="service">Love </span> MUSIC & MOVIES</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="Portfolio photos/full video view.jpg" alt="project7 full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/responsive_full_original.jpg" alt="project7 responsive"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">HTML, CSS, JAVASCRIPT, JQUERY,API</p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://oliviapark113.github.io/project7_group-1/" target="_blank"
+                      title="ragingmongooes SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/project7_group-1" target="_blank"
+                      title="ragingmongooes CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+
+          <div className="row">
+              <h3 className="project-title">WEATHER<br/><span id="service">Dashboard </span></h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="Portfolio photos/wdfull view.jpg" alt="weather_dashboard_06 full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/wdresponsive_full_original.jpg"
+                              alt="weather_dashboard_06 responsive" className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">HTML, CSS, JAVASCRIPT, JQUERY,API</p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://oliviapark113.github.io/weather_dashboard_06/" target="_blank"
+                      title="weather_dashboard_06 SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/weather_dashboard_06" target="_blank"
+                      title="weather_dashboard_06 CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+          
+          <div className="row">
+              <h3 className="project-title">DAILY<br/><span id="service">Real Time </span>SCHEDULER</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="Portfolio photos/scfull view.jpg" alt="realtime_dayPlanner_05 full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/scresponsive_full_original.jpg"
+                              alt="realtime_dayPlanner_05 responsive" className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">HTML, CSS, JAVASCRIPT, JQUERY</p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://oliviapark113.github.io/realtime_dayPlanner_5/" target="_blank"
+                      title="realtime_dayPlanner_05 SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/realtime_dayPlanner_5" target="_blank"
+                      title="realtime_dayPlanner_05 CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+          <div className="row">
+              <h3 className="project-title">LET'S PLAY<br/><span id="service">Timed </span>QUIZ</h3>
+              <div className="myphoto">
+                  <div className="image-wrapper">
+                      <div className="desk-image">
+                          <img src="Portfolio photos/quizfull view.jpg" alt="timed_quiz_04 full screen"
+                              className="img-thumbnail img-fluid"/>
+                      </div>
+                      <div className="mobile-image">
+                          <img src="Portfolio photos/quizresponsive_full_original.jpg"
+                              alt="times_quiz_04 responsive" className="img-thumbnail img-fluid"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-lg-12 col-sm-12">
+                  <p className="techniques">HTML, CSS, JAVASCRIPT</p>
+              </div>
+
+              <div className="col-lg-12 col-sm-12 buttons">
+                  <a href="https://oliviapark113.github.io/timed_quiz_box_4/" target="_blank"
+                      title="timed_quiz_box_4 SITE">
+                      <button id="view-site">VIEW SITE</button>
+                  </a>
+                  <a href="https://github.com/Oliviapark113/timed_quiz_box_4" target="_blank"
+                      title="timed_quiz_box_4 CODE">
+                      <button id="view-code">VIEW CODE</button>
+                  </a>
+              </div>
+          </div>
+
+      </div>
   </div>
+</div>
+
 );
 
 export default Portfolio ;
