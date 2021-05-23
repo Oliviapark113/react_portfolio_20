aa1557f0f7c7d0165afc080de86b798c81a093a6
Olivia Park <oliviaypark113@gmail.com>
2021-05-23 07:00:41 -0400

    updated resume.css

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/resume.css";
-import "../css/style.css";
+// import "../css/style.css";
 import "../pagesComponents/ResumePage/container"
 import Mainbox from "../pagesComponents/ResumePage/mainbox";
 import ResumeContents from "../pagesComponents/ResumePage/resume-contents";
--- src/components/pagesComponents/ResumePage/resume-contents.js
+++ src/components/pagesComponents/ResumePage/resume-contents.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "../../css/resume.css";
-import "../../css/style.css";
+// import "../../css/style.css";
 
 
 const ResumeContents = () => (
     <>
-    <div className="col-md-12 resume-col ">
+    {/* <div className="col-md-12 resume-col "> */}
     <h1 className="resume-name">OLIVIA(YOUNGMEE) PARK </h1>
      <div className="resume-row">
          <p className="my-address">
           322 West 57th APT 43C New York, NY10019 <br />
           Phone: 347-852-1517 | Email: oliviaypark113@gmail.com<br />
-          GitHub:<a href="https://github.com/Oliviapark113" target="_blank" rel="noreferrer" >https://github.com/Oliviapark113</a><br />
-          Portfolio:<a href="https://oliviapark113.github.io/react_portfolio_20/" target="_blank" rel="noreferrer" >https://oliviapark113.github.io/react_portfolio_20/</a><br />
-          LinkedIn:<a href="https://www.linkedin.com/in/olivia-youngmee-park-74858651/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/olivia-youngmee-park-74858651/</a><br />
+          GitHub:<a href="https://github.com/Oliviapark113" target="_blank" rel="noreferrer" className="resume-link" ><i>https://github.com/Oliviapark113</i></a><br />
+          Portfolio:<a href="https://oliviapark113.github.io/react_portfolio_20/" target="_blank" rel="noreferrer" className="resume-link" ><i>https://oliviapark113.github.io/react_portfolio_20/</i></a><br />
+          LinkedIn:<a href="https://www.linkedin.com/in/olivia-youngmee-park-74858651/" target="_blank" rel="noreferrer" className="resume-link" ><i>https://www.linkedin.com/in/olivia-youngmee-park-74858651/</i></a><br />
          </p>
      </div>
      <h3>SUMMARY</h3>
@@ -32,32 +32,32 @@ const ResumeContents = () => (
      </div>
      <h3>PROJECTS</h3>
      <div className="resume-row">
-     <p className="projects"> <span className="project-name">MORNING CUP OF JAVASCRIPT</span><br />
-          Code: <a href="https://github.com/Oliviapark113/morning_cup_of_javascript" target="_blank" rel="noreferrer" >https://github.com/Oliviapark113/morning_cup_of_javascript</a><br />
-          Site: <a href="https://secure-wave-92624.herokuapp.com/ " target="_blank" rel="noreferrer" >https://secure-wave-92624.herokuapp.com/ </a><br />
+     <p className="projects"> <span className="project-name"><i>MORNING CUP OF JAVASCRIPT</i></span><br />
+          Code: <a href="https://github.com/Oliviapark113/morning_cup_of_javascript" target="_blank" rel="noreferrer" className="resume-link"><i>https://github.com/Oliviapark113/morning_cup_of_javascript</i></a><br />
+          Site: <a href="https://secure-wave-92624.herokuapp.com/ " target="_blank" rel="noreferrer" className="resume-link" ><i>https://secure-wave-92624.herokuapp.com/</i></a><br />
     	Built a MERN application to cater users to access latest tech news, coding challenges, and a project inspiration generator.<br />
         Responsible for both client side and server side along with styling.<br /> 
         Used: React, CSS, API, Mongoose, Express, Node.js<br />
          </p>
 
-         <p className="projects"> <span className="project-name">CODE CLOSET</span><br />
-          Code: <a href="https://github.com/Oliviapark113/CodeCloset-P2" target="_blank" rel="noreferrer" >https://github.com/Oliviapark113/CodeCloset-P2</a><br />
-          Site: <a href="https://stormy-stream-41757.herokuapp.com/" target="_blank" rel="noreferrer" >https://stormy-stream-41757.herokuapp.com/</a><br />
+         <p className="projects"> <span className="project-name"><i>CODE CLOSET</i></span><br />
+          Code: <a href="https://github.com/Oliviapark113/CodeCloset-P2" target="_blank" rel="noreferrer"className="resume-link" ><i>https://github.com/Oliviapark113/CodeCloset-P2</i></a><br />
+          Site: <a href="https://stormy-stream-41757.herokuapp.com/" target="_blank" rel="noreferrer" className="resume-link"><i>https://stormy-stream-41757.herokuapp.com/</i></a><br />
           A full stack app to allow users to simulate e-commerce shopping experience where users can select, edit and delete in their choices.<br />
           Responsible for both backend and frontend setting up database and style sketching.<br /> 
-         Used: Handlebars, CSS, jQuery, API, MySQL, Sequelize ORM, Express,Node.js <br />
+         Used: Handlebars, CSS, jQuery, API, MySQL, Sequelize ORM, Express, Node.js <br />
          </p>
 
-         <p className="projects"> <span className="project-name">DAILY DOODLES</span><br />
-          Code: <a href="https://github.com/Oliviapark113/my_daily_doodle_starter" target="_blank" rel="noreferrer" >https://github.com/Oliviapark113/my_daily_doodle_starter</a><br />
-          Site: <a href="https://desolate-harbor-56312.herokuapp.com/" target="_blank" rel="noreferrer">https://desolate-harbor-56312.herokuapp.com/</a><br />
+         <p className="projects"> <span className="project-name"><i>DAILY DOODLES</i></span><br />
+          Code: <a href="https://github.com/Oliviapark113/my_daily_doodle_starter" target="_blank" rel="noreferrer" className="resume-link" ><i>https://github.com/Oliviapark113/my_daily_doodle_starter</i></a><br />
+          Site: <a href="https://desolate-harbor-56312.herokuapp.com/" target="_blank" rel="noreferrer" className="resume-link"><i>https://desolate-harbor-56312.herokuapp.com/</i></a><br />
           	A full stack MERN app to provide users with drawing tools where users can create drawing, edit and delete.<br />
             Used: React, Mongoose, API, Express, Node.js<br /> 
          </p>
 
-         <p className="projects"> <span className="project-name">REACT SYNTH-PAD 2021</span><br />
-          Code: <a href="https://github.com/Oliviapark113/react_synthPad_2021" target="_blank" rel="noreferrer" >https://github.com/Oliviapark113/react_synthPad_2021</a><br />
-          Site: <a href="https://oliviapark113.github.io/react_synthPad_2021/" target="_blank" rel="noreferrer">https://oliviapark113.github.io/react_synthPad_2021/</a><br />
+         <p className="projects"> <span className="project-name"><i>REACT SYNTH-PAD 2021</i></span><br />
+          Code: <a href="https://github.com/Oliviapark113/react_synthPad_2021" target="_blank" rel="noreferrer" className="resume-link" ><i>https://github.com/Oliviapark113/react_synthPad_2021</i></a><br />
+          Site: <a href="https://oliviapark113.github.io/react_synthPad_2021/" target="_blank" rel="noreferrer" className="resume-link"><i>https://oliviapark113.github.io/react_synthPad_2021/</i></a><br />
           Keyboard app for users to select by theme to play sound through keyboard and allow to change volume of sound.<br />
           Used: React<br /> 
          </p>
@@ -65,7 +65,7 @@ const ResumeContents = () => (
      </div>
      <h3>WORK EXPERIENCE</h3>
      <div className="resume-row">   
-     <p className="experiences"> <span className="company-name">Ki pro Sportswear, </span>
+     <p className="experiences"> <span className="company-name"><i>Ki pro Sportswear,</i> </span>
      <span className="position">Head Designer/Sportswear merchandiser</span> <br />
      <span className="year">July 2019-Current</span><span className="location">New York, NY</span><br/>
      Lead all aspects of design and merchandising for boutique women’s activewear for company<br/>
@@ -73,7 +73,7 @@ const ResumeContents = () => (
      <br/>
      </p>
 
-     <p className="experiences"> <span className="company-name">ES Sutton, </span>
+     <p className="experiences"> <span className="company-name"><i>ES Sutton,</i> </span>
      <span className="position">Senior designer for contemporary women’s sportswear</span> <br />
      <span className="year">July 2014 – Sept 2019</span><span className="location">New York, NY</span><br/>
      Managed the entire product design process to include colors, fabric, art direction, trim, sketching, CAD and fits as pertains to styling.<br/>
@@ -87,7 +87,7 @@ const ResumeContents = () => (
      <br/>
      </p>
 
-     <p className="experiences"> <span className="company-name">Sport Elle, </span>
+     <p className="experiences"> <span className="company-name"><i>Sport Elle,</i> </span>
      <span className="position">Senior designer for contemporary women’s sportswear</span> <br />
      <span className="year">Aug 2012 – May 2014</span><span className="location">New York, NY</span><br/>
      Shopped stores for inspirations and researched market trends.<br/>
@@ -99,7 +99,7 @@ const ResumeContents = () => (
      <br/>
      </p>
 
-     <p className="experiences"> <span className="company-name">The Jones Group, </span>
+     <p className="experiences"> <span className="company-name"><i>The Jones Group, </i></span>
      <span className="position">Knitwear Designer for women’s sweaters and knit tops</span> <br />
      <span className="year">Aug 2006– May 2012 </span><span className="location">New York, NY</span><br/>
      Generated detailed technical packages.<br/>
@@ -118,17 +118,17 @@ const ResumeContents = () => (
 
      <h3>EDUCATION</h3>
      <div className="resume-row"> 
-     <p className="education"> <span className="degree">Full Stack Flex Web Development Program</span><br />
+     <p className="education"> <span className="degree"><i>Full Stack Flex Web Development Program</i></span><br />
      <span className="school-name">Northwestern University School of Professional Studies</span> <br /> 
      Evanston, IL <br />                                                  
      Certificate of Completion (May 2021)
      </p>
 
-     <p className="education"> <span className="degree">Associate degree of Arts and Sciences</span><br />
+     <p className="education"> <span className="degree"><i>Associate degree of Arts and Sciences</i></span><br />
      <span className="school-name">Fashion Institute of Technology</span> <br /> 
      New York, NY  <br />                                                  
      </p>
-     <p className="education"> <span className="degree">Bachelor's degree of Arts and Sciences</span><br />
+     <p className="education"> <span className="degree"><i>Bachelor's degree of Arts and Sciences</i></span><br />
      <span className="school-name">Kyung-hee University</span> <br /> 
      Seoul, Korea <br />                                                  
      <br /> 
@@ -137,7 +137,7 @@ const ResumeContents = () => (
 
 
 
-     </div>
+     {/* </div> */}
      </>
 )
 
