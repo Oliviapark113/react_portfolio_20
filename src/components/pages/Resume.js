2aa4653a02afe32b3952137dcff33fe7983c6f70
Olivia Park <oliviaypark113@gmail.com>
2021-05-22 21:30:03 -0400

    updated Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,15 +2,10 @@ import React from "react";
 import "../css/resume.css";
 import "../css/style.css";
 import "../pagesComponents/ResumePage/container"
-import Container from "../pagesComponents/ResumePage/container";
-import Row from "../pagesComponents/ResumePage/row"
 import Mainbox from "../pagesComponents/ResumePage/mainbox";
-import myResumeImg1 from "../../Portfolio_photos/olivia resume-01.jpg"
-import myResumeImg2 from "../../Portfolio_photos/olivia resume-02.jpg"
 import ResumeContents from "../pagesComponents/ResumePage/resume-contents";
 const Resume = () => (
-  
-    
+
         <Mainbox>
          <ResumeContents/>
         </Mainbox>
