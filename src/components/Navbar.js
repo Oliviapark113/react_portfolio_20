929410798f2414b29f5287323539ac39dd8bf558
Olivia Park <oliviaypark113@gmail.com>
2021-05-23 12:50:18 -0400

    update resume.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,41 +14,45 @@ function Navbar({handlePageChange,currentPage,toggleBtn}) {
         <li>
           <a href="#about"
             onClick={() => handlePageChange("About")}
-            classNameName={currentPage === "About" ? "nav-link active" : "nav-link"}>About</a>
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}>About</a>
         </li>
         <li><a href="#contact"
           onClick={() => handlePageChange("Contact")}
-          classNameName={currentPage === "Contact" ? "nav-link active" : "nav-link"}>Contact</a></li>
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>Contact</a>
+          </li>
         <li>
           <a href="#portfolio"
             onClick={() => handlePageChange("Portfolio")}
-            classNameName={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>Portfolio</a>
+            className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>Portfolio</a>
         </li>
+        <li>
+          <a href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={currentPage === "Resume" ? "nav-link active" : "nav-link"}>Resume</a>
+        </li>
+
 
       </ul>
 
-    <ul className="navbar-icons">
+      <ul className="navbar-icons">
         <li className="hvr-icon-grow-rotate"><a href="https://github.com/Oliviapark113" target="_blank"
-                title="olivia's github">
-                <i className="fab fa-github-square hvr-icon"></i>
-            </a></li>
+          title="olivia's github">
+          <i className="fab fa-github-square hvr-icon"></i>
+        </a></li>
         <li className="hvr-icon-grow-rotate">
-            <a href="https://www.linkedin.com/in/olivia-youngmee-park-74858651/" target="_blank"
-                title="olivia's linkedin">
-                <i className="fab fa-linkedin hvr-icon"></i></a>
+          <a href="https://www.linkedin.com/in/olivia-youngmee-park-74858651/" target="_blank"
+            title="olivia's linkedin">
+            <i className="fab fa-linkedin hvr-icon"></i></a>
         </li>
         <li className="hvr-icon-grow-rotate"><a href="mailto:oliviaypark113@gmail.com"
-                title="oliviaypark113@gmail.com"><i className="fas fa-envelope hvr-icon "></i></a> </li>
-
-
-            <li className="hvr-icon-grow-rotate">
-          <a href="#resume"   onClick={() => handlePageChange("Resume")}
-               classNameName={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
-          >
-            <i className="fas fa-file hvr-icon"></i></a> </li>
-
-
-    </ul>
+          title="oliviaypark113@gmail.com"><i className="fas fa-envelope hvr-icon "></i></a>
+          </li>
+        <li className="hvr-icon-grow-rotate">
+          <a href="#resume" onClick={() => handlePageChange("Resume")}
+            title="olivia's resume">
+            <i className="fas fa-file hvr-icon"></i></a>
+            </li>
+      </ul>
 
     <div className="navbar-toggleBtn" onClick={toggleBtn}>
         <i className="fas fa-bars"></i>
--- src/components/pagesComponents/ResumePage/resume-contents.js
+++ src/components/pagesComponents/ResumePage/resume-contents.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../css/resume.css";
-// import "../../css/style.css";
+import "../../css/style.css";
+// import "../../css/navbar.css";
 
 
 const ResumeContents = () => (
@@ -11,6 +12,7 @@ const ResumeContents = () => (
          <p className="my-address">
           322 West 57th APT 43C New York, NY10019 <br />
           Phone: 347-852-1517 | Email: oliviaypark113@gmail.com<br />
+          <br />
           GitHub:<a href="https://github.com/Oliviapark113" target="_blank" rel="noreferrer" className="resume-link" ><i>https://github.com/Oliviapark113</i></a><br />
           Portfolio:<a href="https://oliviapark113.github.io/react_portfolio_20/" target="_blank" rel="noreferrer" className="resume-link" ><i>https://oliviapark113.github.io/react_portfolio_20/</i></a><br />
           LinkedIn:<a href="https://www.linkedin.com/in/olivia-youngmee-park-74858651/" target="_blank" rel="noreferrer" className="resume-link" ><i>https://www.linkedin.com/in/olivia-youngmee-park-74858651/</i></a><br />
